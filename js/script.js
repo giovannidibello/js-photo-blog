3690a8050b6d2ce5cba21e5a3260019ecea52025
Giovanni <dibellogio@gmail.com>
2025-01-24 23:39:40 +0100

    add hover and fullscreen image

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// seleziono elmenti del DOM per output
+// seleziono l'elmento del DOM per output delle card
 const card = document.querySelector(".containerCard");
 
 // chiamata ajax tramite axios
@@ -12,7 +12,7 @@ axios.get("https://lanciweb.github.io/demo/api/pictures/")
         // ciclo su tutta la lunghezza dell'array
         for (i = 0; i < cardObj.length; i++) {
 
-            // destrutturo 
+            // destrutturo dall'elemento iesimo
             const { title, date, url } = cardObj[i];
 
             // stampo in pagina la card
@@ -31,10 +31,44 @@ axios.get("https://lanciweb.github.io/demo/api/pictures/")
                  </div>
             </div>
             `
-
         }
+
+        // selezioni gli elementi del DOM per l'apertura delle immagini fullscreen
+        const imageCard = document.querySelectorAll("figure img");
+        const main = document.querySelector("main");
+
+        // ciclo su tutte le immagini delle cards create in precedenza
+        imageCard.forEach((imageCard) => {
+            // aggiungo l'evento al click
+            imageCard.addEventListener("click", () => {
+
+                // creo un nuovo elemento e gli associo la classe e il display block
+                const openImage = document.createElement("div");
+                openImage.className = "fullScreen";
+                openImage.style.display = "block";
+                openImage.innerHTML = `
+                        <button id="closeButton">Chiudi</button>
+                        <img src="${imageCard.src}" alt="${imageCard.alt}">                         
+                         `;
+
+                // aggiungo il contenitore al main
+                main.appendChild(openImage);
+
+                // seleziono il bottone dal DOM
+                const closeButton = document.getElementById("closeButton");
+                // chiudo il fullscreen al click sul bottone
+                closeButton.addEventListener("click", () => {
+
+                    // imposto di nuovo display none e rimuovo il contenitore
+                    openImage.style.display = "none";
+                    main.removeChild(openImage);
+                });
+            });
+        });
+
 
     })
     .catch(error => {
         console.error(error);
     })
+
